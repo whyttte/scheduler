7682f2d38725bfa8070d76a6f89a908c6bc218b8
favour <fogboruche@yahoo.com>
2020-09-10 15:08:28 -0600

    cleaned up selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,5 @@
 
-export function getAppointmentsForDay(state, day) {
-  
+export function getAppointmentsForDay(state, day) {  
   const theDay = state.days.find( d => d.name === day)
   if(!theDay){
     return []
@@ -9,7 +8,6 @@ export function getAppointmentsForDay(state, day) {
   for(const appointmentId of theDay.appointments){
     result.push(state.appointments[appointmentId])
   }
-  // console.log(result)
   return result
   
 };
@@ -33,19 +31,13 @@ export function getInterview(state, interview) {
   if(!interview){
     return null
   }
-  // console.log(state.interviewers)
   const interviewer = state.interviewers[interview.interviewer]
-  // console.log(interviewer)
   return {...interview, interviewer}
 };
 
 
 
 export function getDays(state) {
-  // go thorugh all the days
-  //for each day get all appointment ids
-  //for each appointment id get appointments
-  //for each day, count empty appointments
   let newDays = [];
   for (const day of state.days) {
     let count = 0;
