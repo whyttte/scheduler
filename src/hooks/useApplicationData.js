d9050e932348cccb523fabe9a20862e53733f271
favour <fogboruche@yahoo.com>
2020-09-10 15:11:02 -0600

    cleaned useapplication data

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -22,7 +22,6 @@ export default function useApplicationData(initial) {
       ...state.appointments,
       [id]: appointment
     };
-    // console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx", interview)
     return axios
 
       .put(`http://localhost:8001/api/appointments/${id}`, {interview})
@@ -47,15 +46,13 @@ export default function useApplicationData(initial) {
 
   
   useEffect(() => {
-
     const first = axios.get(`/api/days`);
     const second = axios.get(`/api/appointments`);
     const third = axios.get(`/api/interviewers`);
 
     Promise.all([first, second, third])
       .then((arrOfValues) => {
-        console.log(".......................................................", arrOfValues)
-        setState(init => ({...init, days: arrOfValues[0].data, appointments: arrOfValues[1].data, interviewers: arrOfValues[2].data}))
+      setState(init => ({...init, days: arrOfValues[0].data, appointments: arrOfValues[1].data, interviewers: arrOfValues[2].data}))
       })
 
 
